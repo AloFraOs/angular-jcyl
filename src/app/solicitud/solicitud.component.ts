9a72b76fb96cd93b487a355f04923093ea1e54c0
Óscar Alonso <oscarjavier.alonso@jcyl.es>
2022-05-05 10:44:47 +0200

    Ejercicio 2
    Definir un atributo en el componente y mostrarlo en el template

--- src/app/solicitud/solicitud.component.ts
+++ src/app/solicitud/solicitud.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SolicitudComponent implements OnInit {
 
+  solicitud = {nombre: "Pedro Picapiedra"};
+
   constructor() { }
 
   ngOnInit(): void {
