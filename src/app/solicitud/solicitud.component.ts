0bb966249a670bb8e1a8e25a8bf026f7582f1ba9
Óscar Alonso <oscarjavier.alonso@jcyl.es>
2022-05-05 11:33:27 +0200

    Ejercicio 6
    Actualizar campos, validar longitud y enviar datos del formulario

--- src/app/solicitud/solicitud.component.ts
+++ src/app/solicitud/solicitud.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SolicitudComponent implements OnInit {
 
-  solicitud = {nombre: "Pedro Picapiedra", valor: ''};
+  solicitud = {nombre:'', apellidos:'', valor: ''};
 
   constructor() {
     setInterval(() => {
@@ -18,7 +18,27 @@ export class SolicitudComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  /* Ejercicio 5 */
   saludar(saludo:string): void {
     alert(saludo)
+  }
+
+  /* Ejercicio 6 */
+  actualizarNombre($event: KeyboardEvent): void {
+    const element = $event.target as HTMLInputElement
+    this.solicitud.nombre = element.value
+  }
+
+  actualizarApellidos($event: KeyboardEvent): void {
+    const element = $event.target as HTMLInputElement
+    this.solicitud.apellidos = element.value
+  }
+
+  validaFormu(): boolean {
+    return this.solicitud.nombre.length < 1 || this.solicitud.apellidos.length < 1
+  }
+
+  enviar(): void {
+    console.info(this.solicitud)
   }
 }
